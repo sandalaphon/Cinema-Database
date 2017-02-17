322f6c30d083825aed4909969c855f4850d2817a
Richard Page-Croft <rpcroft@gmail.com>
2017-02-17 14:58:31 +0000

    Ticket CLass methods read all, delete all and ticket object.delete method added

--- console.rb
+++ console.rb
@@ -5,6 +5,7 @@ require_relative('./models/customer.rb')
 require_relative('./models/ticket.rb')
 
 
+Ticket.delete_all
 Customer.delete_all
 Film.delete_all
 
@@ -39,6 +40,18 @@ ticket1 = Ticket.new({
  'customer_id' => customer2.id
   })
 ticket1.save
+
+ticket2 = Ticket.new({
+ 'film_id' => film1.id,
+ 'customer_id' => customer1.id
+  })
+ticket2.save
+
+ticket3 = Ticket.new({
+ 'film_id' => film2.id,
+ 'customer_id' => customer2.id
+  })
+ticket3.save
 
 binding.pry
 nil
--- models/ticket.rb
+++ models/ticket.rb
@@ -15,16 +15,29 @@ def save()
   @id= SqlRunner.run(sql)[0]['id'].to_i
   end
 
+  def self.all
+    sql = "SELECT * FROM tickets"
+    tickets = SqlRunner.run(sql)
+    return tickets.map{|ticket| Ticket.new(ticket)}
+  end
 
+  def self.delete_all
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
+  end
 
+# update UNTESTED and not currently needed, if another column is added then may uncomment this
 
+  # def update
+  #   sql = "UPDATE tickets SET (film_id, customer_id
+  #   )=(#{@film_id}, #{@customer_id})
+  #   WHERE id = #{@id}"
+  #   SqlRunner.run(sql)
+  # end
 
-
-
-
-
-
-
-
+def delete
+  sql = "DELETE FROM tickets WHERE id = #{@id}"
+  SqlRunner.run(sql)
+end
 
 end
